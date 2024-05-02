c89a12678df81198f00633c9a2e991964cc79d79
FlxwDNS <friedrichsradik@gmail.com>
2024-05-02 21:32:06 +0200

    updated design

--- easycloud-base/src/main/java/net/easycloud/base/group/SimpleGroupHandler.java
+++ easycloud-base/src/main/java/net/easycloud/base/group/SimpleGroupHandler.java
@@ -64,8 +64,8 @@ public final class SimpleGroupHandler implements GroupProvider {
         repository.query().database().findAll().forEach(it -> {
             Base.getInstance().getLogger().log("&7Loaded &9" + it.getName() + " &7as &9" + it.getType().name() + " &7system-group.");
         });
-        if(!repository.query().database().findAll().isEmpty()) {
-            Base.getInstance().getLogger().log("No group was found!");
+        if(repository.query().database().findAll().isEmpty()) {
+            Base.getInstance().getLogger().log("No service-group was found!");
         }
 
         var files = List.of(Path.of(
