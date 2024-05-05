a7a413ec56e5077d2dbf0a5a464b004f7c7628b3
FlxwDNS <friedrichsradik@gmail.com>
2024-05-05 13:56:52 +0200

    added setup

--- easycloud-base/src/main/java/net/easycloud/base/command/defaults/GroupCommand.java
+++ easycloud-base/src/main/java/net/easycloud/base/command/defaults/GroupCommand.java
@@ -1,6 +1,10 @@
 package net.easycloud.base.command.defaults;
 
+import net.bytemc.evelon.DatabaseProtocol;
+import net.bytemc.evelon.cradinates.DatabaseCradinates;
 import net.bytemc.evelon.repository.Filter;
+import net.easycloud.api.conf.DefaultConfiguration;
+import net.easycloud.api.conf.FileHelper;
 import net.easycloud.api.console.LogType;
 import net.easycloud.api.group.Group;
 import net.easycloud.api.group.misc.GroupType;
@@ -9,6 +13,13 @@ import net.easycloud.base.command.CloudCommand;
 import net.easycloud.base.command.Command;
 import net.easycloud.base.Base;
 import net.easycloud.base.service.SimpleServiceHandler;
+import net.easycloud.base.setup.ConsoleSetup;
+import net.easycloud.base.setup.SetupBuilder;
+
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 @Command(name = "group", aliases = {"groups"})
 public final class GroupCommand extends CloudCommand {
@@ -19,7 +30,62 @@ public final class GroupCommand extends CloudCommand {
 
         if (args.length >= 2) {
             if (args[1].equalsIgnoreCase("setup")) {
-                //TODO
+                ConsoleSetup.subscribe(List.of(
+                        SetupBuilder.<String>get()
+                                .key("group.name")
+                                .question("&7What is you group name?")
+                                .build(),
+                        SetupBuilder.<String>get()
+                                .key("group.memory")
+                                .question("&7What is you group memory?")
+                                .build(),
+                        SetupBuilder.<Integer>get()
+                                .key("group.minOnline")
+                                .question("&7What is you group minOnline count?")
+                                .build(),
+                        SetupBuilder.<String>get()
+                                .key("group.maxOnline")
+                                .question("&7What is you group maxOnline count? (-1 = unlimited)")
+                                .build(),
+                        SetupBuilder.<String>get()
+                                .key("group.maxPlayers")
+                                .question("&7What is you group maxPlayers?")
+                                .build(),
+                        SetupBuilder.<String>get()
+                                .key("group.static")
+                                .question("&7Is your group static?")
+                                .build(),
+                        SetupBuilder.<String>get()
+                                .key("group.type")
+                                .question("&7What is you group type?")
+                                .possibleResults(Arrays.stream(GroupType.values()).map(Enum::name).toList())
+                                .build(),
+                        SetupBuilder.<String>get()
+                                .key("group.version")
+                                .question("&7What is you group version?")
+                                .possibleResults(Arrays.stream(GroupVersion.values()).map(Enum::name).toList())
+                                .build()
+                ), values -> {
+                    Base.getInstance().getGroupProvider().getRepository().query().filter(Filter.match("name", values.get("group.name")))
+                            .database()
+                            .findAll().stream().findFirst()
+                            .ifPresentOrElse(group -> {
+                                logger.log("Group " + values.get("group.name") + " already exists!", LogType.WARNING);
+                            }, () -> {
+                                Base.getInstance().getGroupProvider().create(new Group(
+                                        values.get("group.name"),
+                                        Integer.parseInt(values.get("group.memory")),
+                                        Integer.parseInt(values.get("group.minOnline")),
+                                        Integer.parseInt(values.get("group.maxOnline")),
+                                        Integer.parseInt(values.get("group.maxPlayers")),
+                                        Boolean.parseBoolean(values.get("group.static")),
+                                        "ANVIL",
+                                        GroupType.valueOf(values.get("group.type")),
+                                        GroupVersion.valueOf(values.get("group.version"))
+                                ));
+                            });
+                    ((SimpleServiceHandler) Base.getInstance().getServiceProvider()).update();
+                });
                 return;
             }
             if (args[1].equalsIgnoreCase("list")) {
@@ -55,6 +121,24 @@ public final class GroupCommand extends CloudCommand {
                                     logger.log("Group " + args[2] + " already exists!", LogType.WARNING);
                                 }, () -> {
                                     Base.getInstance().getGroupProvider().create(new Group(args[2], Integer.parseInt(args[3]), 1, -1, 50, Boolean.parseBoolean(args[6]), "ANVIL", GroupType.valueOf(args[4].toUpperCase()), GroupVersion.valueOf(args[5].toUpperCase())));
+                                });
+                        ((SimpleServiceHandler) Base.getInstance().getServiceProvider()).update();
+                    } catch (Exception exception) {
+                        logger.log("Please provide valid values!", LogType.ERROR);
+                    }
+                    return;
+                }
+            }
+            if (args[1].equalsIgnoreCase("delete")) {
+                if (args.length == 2) {
+                    try {
+                        Base.getInstance().getGroupProvider().getRepository().query().filter(Filter.match("name", args[2]))
+                                .database()
+                                .findAll().stream().findFirst()
+                                .ifPresentOrElse(group -> {
+                                    //TODO
+                                }, () -> {
+                                    logger.log("Group " + args[2] + " does not exists!", LogType.WARNING);
                                 });
                         ((SimpleServiceHandler) Base.getInstance().getServiceProvider()).update();
                     } catch (Exception exception) {
