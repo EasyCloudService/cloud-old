9c1d65919bfb9683a97cf6d7870429d2f5a70c97
FlxwDNS <friedrichsradik@gmail.com>
2024-05-08 22:45:27 +0200

    removed unused code

--- easycloud-loader/src/main/java/net/easycloud/loader/LoaderBoostrap.java
+++ easycloud-loader/src/main/java/net/easycloud/loader/LoaderBoostrap.java
@@ -14,41 +14,11 @@ public final class LoaderBoostrap {
     public static void main(String[] args) {
         try {
             Path storage = Path.of("storage");
-            Path basePath = Path.of("storage/base.jar");
-
             if(!Files.exists(storage)) {
                 Files.createDirectory(storage);
             }
 
-            /*storage.resolve("jars").toFile().mkdirs();
-            Map.of(
-                    "easycloud-updater.jar", storage.resolve("loader.jar"),
-                    "easycloud-base.jar", basePath,
-                    "easycloud-wrapper.jar", storage.resolve("wrapper.jar"),
-                    "Cloud-API.jar", storage.resolve("jars").resolve("ECS-Plugin.jar")
-            ).forEach((fileName, path) -> {
-                var stream = ClassLoader.getSystemClassLoader().getResourceAsStream(fileName);
-                if(stream != null) {
-                    try {
-                        Files.copy(stream, path, StandardCopyOption.REPLACE_EXISTING);
-                        //new File(ClassLoader.getSystemClassLoader().getResource(fileName).toURI()).delete();
-                    } catch (IOException | URISyntaxException e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-            });*/
-
-            /*Files.copy(Objects.requireNonNull(ClassLoader.getSystemClassLoader().getResourceAsStream("easycloud-updater.jar")), storage.resolve("updater.jar"), StandardCopyOption.REPLACE_EXISTING);
-            Files.copy(Objects.requireNonNull(ClassLoader.getSystemClassLoader().getResourceAsStream("easycloud-base.jar")), basePath, StandardCopyOption.REPLACE_EXISTING);
-            Files.copy(Objects.requireNonNull(ClassLoader.getSystemClassLoader().getResourceAsStream("easycloud-wrapper.jar")), storage.resolve("wrapper.jar"), StandardCopyOption.REPLACE_EXISTING);
-
-            storage.resolve("jars").toFile().mkdirs();
-            Files.copy(Objects.requireNonNull(ClassLoader.getSystemClassLoader().getResourceAsStream("Cloud-API.jar")), storage.resolve("jars").resolve("ECS-Plugin.jar"), StandardCopyOption.REPLACE_EXISTING);
-            */
-
-            //new File(ClassLoader.getSystemClassLoader().getResource("Cloud-API.jar").toURI()).delete();
-
-            final var classLoader = new URLClassLoader(new URL[]{basePath.toUri().toURL()}, ClassLoader.getSystemClassLoader()) {
+            final var classLoader = new URLClassLoader(new URL[]{Path.of("storage/base.jar").toUri().toURL()}, ClassLoader.getSystemClassLoader()) {
                 @Override
                 public void addURL(URL url) {
                     super.addURL(url);
