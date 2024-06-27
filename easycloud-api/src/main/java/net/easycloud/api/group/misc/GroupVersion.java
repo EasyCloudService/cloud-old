65c4ca25e920d6a3595108e855121aa067103639
flxwdns <radik.fr@proton.me>
2024-06-27 18:28:38 +0200

    make some updates

--- easycloud-api/src/main/java/net/easycloud/api/group/misc/GroupVersion.java
+++ easycloud-api/src/main/java/net/easycloud/api/group/misc/GroupVersion.java
@@ -13,8 +13,8 @@ public enum GroupVersion {
     PAPER_1_20_1("https://api.papermc.io/v2/projects/paper/versions/1.20.1/builds/196/downloads/paper-1.20.1-196.jar"),
     PAPER_1_20_2("https://api.papermc.io/v2/projects/paper/versions/1.20.2/builds/318/downloads/paper-1.20.2-318.jar"),
     PAPER_1_20_4("https://api.papermc.io/v2/projects/paper/versions/1.20.4/builds/496/downloads/paper-1.20.4-496.jar"),
-    PAPER_1_20_5("https://api.papermc.io/v2/projects/paper/versions/1.20.4/builds/381/downloads/paper-1.20.4-381.jar"),
-    PAPER_1_20_6("https://api.papermc.io/v2/projects/paper/versions/1.20.6/builds/32/downloads/paper-1.20.6-32.jar"),
+    PAPER_1_20_6("https://api.papermc.io/v2/projects/paper/versions/1.20.6/builds/147/downloads/paper-1.20.6-147.jar"),
+    PAPER_1_21("https://api.papermc.io/v2/projects/paper/versions/1.21/builds/37/downloads/paper-1.21-37.jar"),
     VELOCITY_LATEST("https://api.papermc.io/v2/projects/velocity/versions/3.3.0-SNAPSHOT/builds/386/downloads/velocity-3.3.0-SNAPSHOT-386.jar");
 
     private final String url;
