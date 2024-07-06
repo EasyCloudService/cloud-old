b6ee6f608da81d9e2e13f15f66ef81235e412b79
Radik Friedrichs <radik.fr@proton.me>
2024-07-06 14:13:58 +0200

    updated versions

--- easycloud-api/src/main/java/net/easycloud/api/group/misc/GroupVersion.java
+++ easycloud-api/src/main/java/net/easycloud/api/group/misc/GroupVersion.java
@@ -13,9 +13,9 @@ public enum GroupVersion {
     PAPER_1_20_1("https://api.papermc.io/v2/projects/paper/versions/1.20.1/builds/196/downloads/paper-1.20.1-196.jar"),
     PAPER_1_20_2("https://api.papermc.io/v2/projects/paper/versions/1.20.2/builds/318/downloads/paper-1.20.2-318.jar"),
     PAPER_1_20_4("https://api.papermc.io/v2/projects/paper/versions/1.20.4/builds/496/downloads/paper-1.20.4-496.jar"),
-    PAPER_1_20_6("https://api.papermc.io/v2/projects/paper/versions/1.20.6/builds/147/downloads/paper-1.20.6-147.jar"),
-    PAPER_1_21("https://api.papermc.io/v2/projects/paper/versions/1.21/builds/37/downloads/paper-1.21-37.jar"),
-    VELOCITY_LATEST("https://api.papermc.io/v2/projects/velocity/versions/3.3.0-SNAPSHOT/builds/400/downloads/velocity-3.3.0-SNAPSHOT-400.jar");
+    PAPER_1_20_6("https://api.papermc.io/v2/projects/paper/versions/1.20.6/builds/148/downloads/paper-1.20.6-148.jar"),
+    PAPER_1_21("https://api.papermc.io/v2/projects/paper/versions/1.21/builds/44/downloads/paper-1.21-44.jar"),
+    VELOCITY_LATEST("https://api.papermc.io/v2/projects/velocity/versions/3.3.0-SNAPSHOT/builds/404/downloads/velocity-3.3.0-SNAPSHOT-404.jar");
 
     private final String url;
 }
